5df6513f641948d9cec7eea3a9c4cafbd341a4ab
Harish Naik <harishgnaik@gmail.com>
2026-02-04 15:22:09 -0600

    download

--- python/nb/marimo/fill_simulator.py
+++ python/nb/marimo/fill_simulator.py
@@ -22,20 +22,22 @@ def _():
 
     import databento as db
     import polars as pl
+
     return Path, db, json, pl
 
 
 @app.cell
 def _(Path, json):
-    data_dir = Path().absolute() / 'extdata'
+    data_dir = Path().absolute() / "extdata"
 
     def load_json(name: str) -> dict:
-        path = data_dir / f'{name}.json'
+        path = data_dir / f"{name}.json"
         return json.load(path.open())
 
     def print_dict(data: dict):
         for k, v in data.items():
             print(k, v)
+
     return (data_dir,)
 
 
@@ -44,22 +46,22 @@ def _(Path, db, pl):
     def make_dataframe(path: Path) -> pl.DataFrame:
         data = db.read_dbn(path)
         return pl.from_pandas(data.to_df())
+
     return (make_dataframe,)
 
 
 @app.cell
 def _(data_dir, make_dataframe, pl):
     def basic_explore(df_orig: pl.DataFrame, symbol: str):
-        df = df_orig.filter(pl.col('symbol') == symbol.upper())
+        df = df_orig.filter(pl.col("symbol") == symbol.upper())
         print(df.head())
 
-
-    for file_path in sorted(data_dir.glob('*.zst')):
-        pq_out_path = file_path.parent / f'{file_path.stem}.parquet'
+    for file_path in sorted(data_dir.glob("*.zst")):
+        pq_out_path = file_path.parent / f"{file_path.stem}.parquet"
         if pq_out_path.exists():
             continue
         df = make_dataframe(file_path)
-        print(f'loaded dataframe from {file_path} of dim:{df.shape}')
+        print(f"loaded dataframe from {file_path} of dim:{df.shape}")
         df.write_parquet(pq_out_path)
     return
 

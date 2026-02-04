9c734a4311e0220c764dcd8437514ae2ad04ea50
Harish Naik <harishgnaik@gmail.com>
2026-02-04 15:14:11 +0000

    WIP

--- python/nb/marimo/fill_simulator.py
+++ python/nb/marimo/fill_simulator.py
@@ -1,7 +1,27 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "marimo>=0.19.0",
+#     "pyzmq>=27.1.0",
+# ]
+# ///
+
 import marimo
 
+__generated_with = "0.19.7"
 app = marimo.App()
+
+
+@app.cell
+def _():
+    import databento as db
+    return
+
 
 @app.cell
 def _():
     return
+
+
+if __name__ == "__main__":
+    app.run()
